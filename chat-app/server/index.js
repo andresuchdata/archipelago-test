a5df85f497f02371771b8bed5b890336c42c652b
Andre Suchitra <andresuch.data@gmail.com>
2024-11-11 11:39:24 +0700

    secure cors setting

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -1,13 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "http://simple-chat-arch.s3-website-ap-southeast-1.amazonaws.com",
-      "https://simple-chat-arch.netlify.app"
-    ],
+    origin: process.env.ALLOWED_ORIGINS?.split(',') || ["http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true
   },
